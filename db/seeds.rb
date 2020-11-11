9196b37b2247d3965e8d359c00d293cc5305bf1a
VictorCt <victor.c.boncoin@gmail.com>
2020-11-11 19:29:05 +0100

    Avancement

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,22 +11,39 @@ User.destroy_all
 user1 = User.create!(email: "vic@test", pseudo: "Vic", password: "111111", password_confirmation: "111111")
 
 
-recette1 = Recette.create!(titre: "Pizza pepperoni", description: "Préchauffer le four à 200°C (thermostat 6-7).
-Mélanger le coulis de tomate avec le 4 épices, saler et poivrer si besoin.
-Egoutter la mozzarella et la couper en tranches.
-Dérouler la pâte à pizza et la placer sur une plaque allant au four en conservant le papier cuisson dessous.
-Badigeonner de coulis de tomate et répartir les tranches de mozzarella.
-Enfourner 15 minutes.
-Ajouter les tranches de pepperoni et les feuilles de basilic.
-Enfourner à nouveau 5 minutes.", univers:"Pizza", typeplat:"plat", lienUrl:"https://www.marmiton.org/recettes/recette_pizza-pepperoni_371851.aspx#d89655-p1", commentaire: "Une recette de dingue !", note:"5", duree: "40", difficulte: "1", cuisson: "5", user_id: user1.id)
+recette1 = Recette.create!(titre: "The Foccacia", description: "LA PRÉPARATION DE LA RECETTE
+Pour la pâte :
+Mettre la levure et l'eau tiède dans le bol et mélanger.
+Ajoutez la farine, puis l'huile et le sel et pétrir 4mn jusqu'à obtenir une pâte bien souple.
+Mettez la pâte en boule, huilez-la légèrement, déposez-la dans un saladier, couvrez et laissez reposer 10mn.
+Déposez ensuite délicatement la pâte sur une plaque de cuisson recouvert de papier sulfurisé, et laissez lever encore 10mn.
+Puis étalez délicatement la pâte à l'aide d'un rouleau à pâtisserie, sans appuyer pour ne pas détruire les bulles d'air.
+Laissez à nouveau la pâte se détendre 20mn.
+
+Pour l'émultion:
+Mettre l'eau, l'huile d'olive et le sel dans le bol et mélanger activement.
+
+Le façonnage:
+Au bout des 20mn, faîtes des trous avec les doigts en appuyant fortement, et versez  l'émulsion huile/eau/sel sur la pâte, afin que tous les trous soient bien remplis , et laissez à nouveau lever 20mn et saupoudrez de romarin.
+10 minutes avant la fin de ce dernier temps de pose, préchauffez votre four à 200° chaleur tournante
+Enfournez la foccacia lorsqu'elle a fini de lever, pour une durée de 20mn.
+Elle doit être juste dorée.
+Surveillez bien la fin de cuisson.", univers:"Pâte", typeplat:"Apéritif", lienUrl:"https://www.espace-recettes.fr/pains-viennoiseries-recettes/la-foccacia/ghi054ta-e6628-405718-cfcd2-75w2hxw6", commentaire: "Une recette de dingue !", duree: "40", difficulte: "20", cuisson: "5", user_id: user1.id)
 
 
 
-ingredient1 = Ingredient.create!(nom:"pâte à pizza", quantite:"1", unite:"unité(s)",recette_id: recette1.id)
-ingredient2 = Ingredient.create!(recette_id: recette1.id, nom:"pepperoni", quantite:"150", unite:"g")
-ingredient3 = Ingredient.create!(recette_id: recette1.id, nom:"Boules de mozzarella", quantite:"2", unite:"unité(s)")
-ingredient4 = Ingredient.create!(recette_id: recette1.id, nom:"coulis de tomate", quantite:"200", unite:"g")
+ingredient1 = Ingredient.create!(nom:"levure fraîche", quantite:"20", unite:"G",recette_id: recette1.id)
+ingredient2 = Ingredient.create!(recette_id: recette1.id, nom:"Eau", quantite:"300", unite:"g")
+ingredient3 = Ingredient.create!(recette_id: recette1.id, nom:"Farine", quantite:"500", unite:"g")
+ingredient4 = Ingredient.create!(recette_id: recette1.id, nom:"Huile d'olive", quantite:"15", unite:"g")
+ingredient4 = Ingredient.create!(recette_id: recette1.id, nom:"Sel", quantite:"10", unite:"g")
+ingredient2 = Ingredient.create!(recette_id: recette1.id, nom:"Eau", quantite:"30", unite:"g")
+ingredient2 = Ingredient.create!(recette_id: recette1.id, nom:"Sel", quantite:"1", unite:"Cuillière à café")
+ingredient2 = Ingredient.create!(recette_id: recette1.id, nom:"Romarin", quantite:"1", unite:"unité(s)")
+
+
 ingredient5 = Ingredient.create!(recette_id: recette1.id, nom:"basilic", quantite:"1/2", unite:"unité(s)")
+ingredient4 = Ingredient.create!(recette_id: recette1.id, nom:"Huile d'olive", quantite:"30", unite:"g")
 
 recette2 = Recette.create!(titre: "spaguetti Bolo(sse)", description: "Préchauffer le four à 200°C (thermostat 6-7).
 Mélanger le coulis de tomate avec le 4 épices, saler et poivrer si besoin.
@@ -85,7 +102,7 @@ ingredient3 = Ingredient.create!(recette_id: recette4.id, nom:"Sel", quantite:"2
 
 require "open-uri"
 
-file1 = URI.open ('https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80')
+file1 = URI.open ('hhttps://fr.rc-cdn.community.thermomix.com/recipeimage/ghi054ta-e6628-405718-cfcd2-75w2hxw6/a8479e31-f2ad-417d-b2f0-62267b2c0680/main/la-foccacia.jpg')
 recette1.photo.attach(io: file1, filename:"pizza.png", content_type: 'image/png')
 
 file2 = URI.open ('https://images.unsplash.com/photo-1571175534150-72cd2b5a6039?ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80')

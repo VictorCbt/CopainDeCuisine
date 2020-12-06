7ef13b642ca9061fd3a9c7ccc3fb14de70637902
VictorCt <victor.c.boncoin@gmail.com>
2020-12-06 08:52:20 +0100

    JS

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,12 +3,14 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
     const bannerElement = document.getElementById('banner-typed-text');
 
-  new Typed('#banner-typed-text', {
-    strings: ["tes copains", "ta famille"],
+ if (bannerElement) { // only build a map if there's a div#map to inject into
+    new Typed(bannerElement, {
+    strings:["tes copains", "ta famille"],
     typeSpeed: 125,
     loop: true
-  });
-}
+    });
+  }
+};
 
 export { loadDynamicBannerText };
 
